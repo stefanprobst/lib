b84a79801fe82d5919a492615566e226e37846a6
Stefan Probst <stefanprobst@posteo.de>
2022-12-29 16:45:20 +0100

    feat: add url-search-params package

---
+++ packages/url-search-params/src/index.ts
@@ -0,0 +1,25 @@
+type Nullable<T> = T | null | undefined;
+
+type Primitive = boolean | number | string;
+
+export interface CreateUrlSearchParamsParams {
+	[key: string]: Nullable<Primitive> | Array<Nullable<Primitive>>;
+}
+
+export function createUrlSearchParams(params: CreateUrlSearchParamsParams) {
+	const urlSearchParams = new URLSearchParams();
+
+	for (const [key, value] of Object.entries(params)) {
+		if (Array.isArray(value)) {
+			value.forEach((v) => {
+				if (v != null) {
+					urlSearchParams.append(key, String(v));
+				}
+			});
+		} else if (value != null) {
+			urlSearchParams.set(key, String(value));
+		}
+	}
+
+	return urlSearchParams;
+}
