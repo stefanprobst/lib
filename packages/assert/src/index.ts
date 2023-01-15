3113794421a70c2e1ac170bf093ee1743f1e4690
Stefan Probst <stefanprobst@posteo.de>
2023-01-15 13:08:07 +0100

    feat: move packages to monorepo

--- packages/assert/src/index.ts
+++ packages/assert/src/index.ts
@@ -1,12 +1,13 @@
+const defaultMessage = "Assertion failed";
+const isProduction = process.env["NODE_ENV"] === "production";
+
 /**
  * @see https://gist.github.com/jamiebuilds/2a40f565cba317a80bed1eec127340f6
  */
 export function assert(value: boolean, message?: string): asserts value;
 export function assert<T>(value: T | null | undefined, message?: string): asserts value is T;
 export function assert(value: unknown, message?: string): void {
-	if (value === false || value == null) {
-		if (process.env["NODE_ENV"] !== "production") {
-			throw new Error(message || "Assertion failed");
-		}
-	}
+	if (value !== false && value != null) return;
+	const msg = isProduction ? defaultMessage : message ?? defaultMessage;
+	throw new Error(msg);
 }
